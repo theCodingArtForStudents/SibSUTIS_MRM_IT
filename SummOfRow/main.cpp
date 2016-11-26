e5c9d22d491b2d29577652f283f261ce065eed7b
Yaroslav <khaidayev@gmail.com>
2016-11-27 00:45:37 +0700

    implemented an computing

--- SummOfRow/main.cpp
+++ SummOfRow/main.cpp
@@ -3,13 +3,40 @@
 
 using namespace std;
 
-int main(int argc, char *argv[])
+int main()
 {
-    cout << "Hello World!" << endl;
+  cout << "Hello World!" << endl;
 
-    int e = pow(10, -4);
+  int e = pow(10, -4);
 
-    //------------------------
+  //------------------------
 
-    return 0;
+  long double an = 0;
+  int n = 1;
+  long double nfact = 1;
+
+  for (int i = n; i > 0; --i) {
+    nfact *= i;
+  }
+  cout << "nfact :" << nfact << endl;
+  an = 2 * pow(nfact, 2);
+  cout << "an = 2(n!)^2 : " << an << endl;
+  nfact = 1;
+  for (int i = 2*n; i > 0; --i) {
+    nfact *= i;
+  }
+  cout << "(2n)! : " << nfact << endl;
+  n = 3 * nfact;
+
+  nfact = 1;
+  long double size = pow(2, sizeof(long double) * sizeof(long double));
+  for (long double i = n;  (nfact < size) ? (i > 0) : (nfact <= size); --i) {
+    nfact *= i;
+  }
+
+  an = an / nfact;
+
+  cout << "an = 2(n!)^2 / (3((2n)!)! == " << an << endl;
+
+  return 0;
 }
