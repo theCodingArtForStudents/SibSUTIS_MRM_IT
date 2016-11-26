17ea03e7c34e8ac59a6b28f1d8479976d8976aac
Yaroslav <khaidayev@gmail.com>
2016-11-26 21:50:08 +0700

    firs project

---
+++ SummOfRow/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    cout << "Hello World!" << endl;
+
+    int e = pow(10, -4);
+
+    //------------------------
+
+    return 0;
+}
