90caffcfc070202506f6c25050df976b778da63b
Slavjan <khaidayev@gmail.com>
2017-12-11 22:16:47 +0700

    fix of summ

--- summOfRaw/tochnost.cpp
+++ summOfRaw/tochnost.cpp
@@ -5,21 +5,45 @@ using namespace std;
 
 int main()
 {
-  int n = 0;
-  double sum = 0;
-  cout << "Please enter number: ";
-  cin >> n;
+  uint64_t n = 0; // длинный-длинный беззнаковый целый
+  // максимальное значение 2^64
+  double sum = 0, // переменная для суммы по условию задачи
+         sum2 = 0, // для суммы с взаимнообратным членом ряда
+         an = 1, // общий член ряда
+         eps = pow(10, 4), // 10^4
+         eps2 = pow(10, -4) ; // 10^-4
 
-  int k = 1;
-  for( int i = n; i > 0; i-- )
-    k *= i;
+// (10^n) / n!
+  while( an < eps ) // 10^n растет быстрее чем n!
+  {// поэтому общий член ряда всегда возрастает и не может стать < 10^-4
+    uint64_t k = 1;
+    sum += an; // прибавляем общий член ряда к сумме
 
-  do
-  {
-    sum += pow( 10, n)/k;
+    for( uint64_t i = n; i > 0; i-- ) // n! == k
+      k *= i; // получаем факториал
+
+    double po = pow( 10, n);// возводим 10 в степень n
+    an = po/k; // получаем общий член рада == (10^n)/ n!
+    n++;
   }
-  while( sum < pow(10, -4) );
+
+  an = 1;
+  n = 0;
+  // a если n! / 10^n
+  while( an > eps2 ) //  то an совсем скоро станет <= 10^-4
+  {
+    uint64_t k = 1;
+    sum2 += an;
+
+    for( uint64_t i = n; i > 0; i-- ) // n! == k
+      k *= i;
+
+    double po = pow( 10, n);
+    an = k/po;
+    n++;
+  }
 
   cout << endl << "summ is: " << sum << endl;
+  cout << endl << "summ is: " << sum2 << endl;
   return 0;
 }
