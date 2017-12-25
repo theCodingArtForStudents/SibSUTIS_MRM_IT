29847d96de4f317c5b5e0d366d1af098f48e9268
Slavjan <khaidayev@gmail.com>
2017-12-26 01:47:37 +0700

    rewrite without stl

--- singleWordString/main.cpp
+++ singleWordString/main.cpp
@@ -1,47 +1,87 @@
 #include <iostream>
-#include <map>
+#include <string.h>
 
 using namespace std;
 
 int main()
 {
-  string input;
+  char input[] = "Lorem ipsum ipsum dolor sit sit amet, "
+                 "consectetur adipiscing elit, "
+                 "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. "
+                 "Ut enim ad minim veniam, quis nostrud exercitation ullamco "
+                 "laboris nisi ut aliquip ex ea commodo consequat. ";
 
-  cout << "Please enter your string: ";
-
-  getline(cin, input);
-
-  if( input.back() != '.'){
-    cout << "You forgot '.' !" << endl;
-    input.push_back('.');
+  int wordsCount = 0;
+  int i = 0;
+  while ( input[i] != '\0') {
+    if( input[i] == ' '){
+      wordsCount++;
+    }
+    i++;
   }
-  cout << "Your string: \"" << input << '\"' << endl << endl;
 
-  map<string, int> words;
+  char** words = new char* [wordsCount];
 
-  string word;
-  for( auto letter : input ){
-    if( letter != ' ' && letter != '.' ){
-      word.push_back(letter);
-      continue;
+  int handleLeft = 0;
+  for (int i = 0; i < wordsCount; ++i) {
+
+    int symbolsCount = 0;
+    while ( input[handleLeft] != ' ') {
+
+      if( input[handleLeft] != ' '
+          && input[handleLeft] != '.'
+          && input[handleLeft] != ','){
+
+        symbolsCount++;
+      }
+
+      handleLeft++;
     }
 
-    if( words.find(word) == words.end() )
-      words[word] = 1;
-    else
-      words[word]++;
+//    cout << "symbolsCount " << symbolsCount << " input << endl;
+    handleLeft++;
 
-    word = "";
+
+    words[i] = new char [symbolsCount+1];
+
+    int handleRight = handleLeft-1;
+
+    words[i][symbolsCount] = '\0';
+    while (handleRight >= 0
+           || input[handleRight] != ' ') {
+
+      if(input[handleRight] != '.'
+         && input[handleRight] != ' '
+         && input[handleRight] != ','
+         && symbolsCount >= 0){
+
+        symbolsCount--;
+        words[i][symbolsCount] = input[handleRight];
+
+      }
+
+      handleRight--;
+    }
+  }
+
+  int *substrCount = new int [wordsCount];
+  for (int i = 0; i < wordsCount; ++i) {
+    substrCount[i] = 0;
+    for (int j = 0; j < wordsCount; ++j) {
+      if( strcmp(words[i], words[j]) == 0 ){
+        substrCount[i]++;
+      }
+    }
+    cout << "word[" << i << "]" << words[i] << endl;
   }
 
   cout << "Alone in the darkness: [" << endl;
-  for( auto _word : words){
-    if( _word.second == 1){
-      cout << "\t\"" << _word.first << "\"," << endl;
+  for (int i = 0; i < wordsCount; ++i) {
+    if(substrCount[i] == 1){
+      cout << i << "\t\"" << words[i] << "\"," << endl;
     }
   }
   cout << "]" << endl;
-
 
   return 0;
 }
