4ea67836f62a96be429d336a596ca149dd4a4fa7
Slavjan <khaidayev@gmail.com>
2017-12-26 03:12:08 +0700

    fix

--- singleWordString/main.cpp
+++ singleWordString/main.cpp
@@ -7,8 +7,10 @@ int main()
 {
   char input[] = "Lorem ipsum ipsum dolor sit sit amet, "
                  "consectetur adipiscing elit, "
+                 "consectetur adipiscing elit, "
                  "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. "
                  "Ut enim ad minim veniam, quis nostrud exercitation ullamco "
+                 "laboris nisi ut aliquip ex ea commodo consequat. "
                  "laboris nisi ut aliquip ex ea commodo consequat. ";
 
   int wordsCount = 0;
