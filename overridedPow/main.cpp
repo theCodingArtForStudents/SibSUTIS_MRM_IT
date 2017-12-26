21c6b68aa2dbe8913c559df9c8c9d129e3c704d7
Slavjan <khaidayev@gmail.com>
2017-12-26 15:42:21 +0700

    comment

--- overridedPow/main.cpp
+++ overridedPow/main.cpp
@@ -1,22 +1,22 @@
 #include <iostream>
 
 using namespace std;
-
+// возведение в степень
 int64_t overPow(int value, size_t power){
-  int64_t result = 1;
+  int64_t result = 1; // будущий результат
 
   for (size_t i = 0; i < power; ++i) {
-    result *= value;
+    result *= value; // домнодаем число на себя столько раз какая степень указана
   }
 
-  return result;
+  return result;// возвращаем резкльтат наружу
 }
 
 int64_t overRoot( int value, size_t power ){
   int64_t result = 1;
 
-  while ( value != overPow(result, power)) {
-    result++;
+  while ( value != overPow(result, power)) { // пока результат возведенный в указанную степень не сравняется с числом
+    result++; // увеличиваем число на 1
   }
 
   return result;
@@ -27,8 +27,8 @@ int main()
 
   int result = 0;
 
-  cout << "5^3  " << overPow(5, 3) << endl;
-  cout << "2\\/121   " << overRoot(121, 2) << endl;
+  cout << "5^3  " << overPow(5, 3) << endl; // выводим 5 в степени 3
+  cout << "2\\/121   " << overRoot(121, 2) << endl; // ищем корень и 121
 
   return 0;
 }
