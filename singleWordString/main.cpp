7558b5e662b92b2c7f8cef0f1f52bee2a6f156db
Yaroslav <khaidayev@gmail.com>
2018-02-28 22:55:12 +0700

    comments added

--- singleWordString/main.cpp
+++ singleWordString/main.cpp
@@ -13,48 +13,44 @@ int main()
                  "laboris nisi ut aliquip ex ea commodo consequat. "
                  "laboris nisi ut aliquip ex ea commodo consequat. ";
 
-  size_t wordsCount = 0;
-  int i = 0;
-  while ( input[i] != '\0') {
-    if( input[i] == ' '){
-      wordsCount++;
+  size_t wordsCount = 0; // variable for amount of words
+  size_t i = 0; // counter for next loop
+  while ( input[i] != '\0') { // цикл от начала строки до тех пор, пока не встретим нуль-символ
+    if( input[i] == ' '){ // привстрече пробела
+      wordsCount++; // увеличиваем количество найденных слов
     }
-    i++;
+    i++; // номер следующей итерации 
   }
 
-  char** words = new char* [wordsCount];
-  char *lexema = strtok(input, ", .-");
+  char** words = new char* [wordsCount]; // динамический символьный* массив для слов с размером в [количество слов]
+  char *lexema = strtok(input, ", .-"); // выделение слов из строки input в массив lexemа c разграничением по разделителю
 
   for (size_t i = 0; i < wordsCount; ++i) {
-    size_t symbolsCount = strlen(lexema);
+    size_t symbolsCount = strlen(lexema); // узнаём размер текущего слова
 
-    words[i] = new char [symbolsCount];
-    words[i] = lexema;
+    words[i] = new char [symbolsCount]; // выделение памяти под количество символов в текущем слове
+    words[i] = lexema; // передача слова в массив
 
-    lexema = strtok(NULL, ", .-");
+    lexema = strtok(NULL, ", .-"); // выделение следующего слова из исходной строки
   }
 
-//  cout << words[0] << endl;
-
-
-  int *substrCount = new int [wordsCount];
-  for (size_t i = 0; i < wordsCount; ++i) {
-    substrCount[i] = 0;
-    for (size_t j = 0; j < wordsCount; ++j) {
-      if( strcmp(words[i], words[j]) == 0 ){
-        substrCount[i]++;
+  int *substrCount = new int [wordsCount]; // динамический массив для подсчета количества повторений слов
+  for (size_t i = 0; i < wordsCount; ++i) { // начинаем цикл с нулевого слова
+    substrCount[i] = 0; // изначално текущее слово не повторялось ни разу
+    for (size_t j = 0; j < wordsCount; ++j) { // цикл для нахождения повторов
+      if( strcmp(words[i], words[j]) == 0 ){ // берем очередное слово и сравниваем со всеми остальными
+        substrCount[i]++; // если найдено повторение увеличиваем счетчик
       }
     }
-    cout << "word[" << i << "]" << words[i] << endl;
   }
 
-  cout << "Alone in the darkness: [" << endl;
-  for (size_t i = 0; i < wordsCount; ++i) {
-    if(substrCount[i] == 1){
-      cout << i << "\t\"" << words[i] << "\"," << endl;
+  cout << "Alone in the darkness: [" << endl; // одинокие во тьме
+  for (size_t i = 0; i < wordsCount; ++i) {// цикл для вывода слов-одиночек
+    if(substrCount[i] == 1){ // если текущее слово не повторялось 
+      cout << i << "\t\"" << words[i] << "\"," << endl; // выводим это слово
     }
   }
-  cout << "]" << endl;
+  cout << "]" << endl; // конец вывода
 
   return 0;
 }
